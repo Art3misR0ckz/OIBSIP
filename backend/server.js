9a8a219d4c9877b56cf1febc82beff273c5d63a1
Anushka Sahoo <anusahoo1612@gmail.com>
2026-05-21 00:54:08 +0530

    Initial full stack setup

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+
+// routes
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+
+// connect database
+connectDB();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use("/api/auth", authRoutes);
+
+// test route
+app.get("/", (req, res) => {
+    res.send("API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
